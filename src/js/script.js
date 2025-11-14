e4d36487238e7fed91b9f3c0350036c8e71dfc9c
LauraBailly08 <laura.bailly02@edu.univ-fcomte.fr>
2025-11-14 10:43:17 +0100

    ouverture menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,11 @@
-nav = document.querySelector(".menu");
-nav.ariaHidden = "false";
+const nav = document.querySelector(".menu");
+
 const toggle = document.querySelector(".menu-btn");
+
+toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+    toggle.ariaExpanded = isClosed ? "true" : "false";
+    nav.ariaHidden = isClosed ? "false" : "true";
+});
